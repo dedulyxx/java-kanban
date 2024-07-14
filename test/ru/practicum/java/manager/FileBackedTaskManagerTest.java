911d6c34b2a9e2c44e803a478b77079d0b720177
dedulyxx <dedulyxx@yandex.ru>
2024-07-14 20:50:13 +0300

    Внесены правки согласно комментариям

--- test/ru/practicum/java/manager/FileBackedTaskManagerTest.java
+++ test/ru/practicum/java/manager/FileBackedTaskManagerTest.java
@@ -6,33 +6,33 @@ import ru.practicum.java.tasks.Subtask;
 import ru.practicum.java.tasks.Task;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileBackedTaskManagerTest {
 
-    File file = Paths.get("/home/tasks.txt").toFile();
+    private Path path = Paths.get("./count.txt");
 
     @Test
     void loadEmptyFile() throws IOException {
-        FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(file);
+        FileBackedTaskManager fileBackedTaskManager = FileBackedTaskManager.loadFromFile(path.toFile());
         assertEquals(fileBackedTaskManager, fileBackedTaskManager);
     }
 
     @Test
     void saveTasksInFile() throws IOException {
-        TaskManager fileManager = new FileBackedTaskManager(file);
+        TaskManager fileManager = new FileBackedTaskManager(path.toFile());
         Task taskOne = new Task("Задача 1", "задача 1 - описание");
         Epic epic = new Epic("Epic 1", "Epic 1 - description");
         Subtask subtask = new Subtask("Subtask 1", "Subtask 1", 1);
         fileManager.addTask(taskOne);
         fileManager.addEpic(epic);
         fileManager.addNewSubtask(subtask);
-        BufferedReader br = new BufferedReader(new FileReader("/home/tasks.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("./count.txt"));
         String line;
         int i = 0;
         while ((line = br.readLine()) != null) {
@@ -43,8 +43,8 @@ class FileBackedTaskManagerTest {
 
     @Test
     void loadTasksFromFile() throws IOException {
-        FileBackedTaskManager.loadFromFile(file);
-        BufferedReader br = new BufferedReader(new FileReader("/home/tasks.txt"));
+        FileBackedTaskManager.loadFromFile(path.toFile());
+        BufferedReader br = new BufferedReader(new FileReader("./count.txt"));
         int i = 0;
         while (br.readLine() != null) {
             i++;
